dc4d527559933965f3b70b039ab409d130f1ecd8
Nestor Cahuana <120668823+nestorkira@users.noreply.github.com>
2026-08-25 03:57:46 -0500

    25/08

--- app.py
+++ app.py
@@ -104,13 +104,13 @@ CONFIG_EQUIPOS = {
     "TD079": {"orden": 8,  "grupo": "DTH"},
     "TD080": {"orden": 9,  "grupo": "DTH"},
     "TD081": {"orden":10,  "grupo": "DTH"},
+    "TD082": {"orden":11,  "grupo": "DTH"},
 
-    "TD091": {"orden":11,  "grupo": "RTR"},
-    "TD092": {"orden":12,  "grupo": "RTR"},
+    "TD091": {"orden":13,  "grupo": "RTR"},
+    "TD092": {"orden":14,  "grupo": "RTR"},
     
-    "TD030": {"orden":13,  "grupo": "FUERA"},
-    "TD072": {"orden":14,  "grupo": "FUERA"},
-    "TD082": {"orden":15,  "grupo": "FUERA"},
+    "TD030": {"orden":14,  "grupo": "FUERA"},
+    "TD072": {"orden":15,  "grupo": "FUERA"},
     "TD083": {"orden":16,  "grupo": "FUERA"},
 
 }
